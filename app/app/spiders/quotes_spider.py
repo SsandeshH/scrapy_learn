e9974eb6f850137bf7dd0f8c3da50e8c6be4a287
San <ssandeshupretii@gmail.com>
2026-01-15 20:08:52 +0545

    pagination handled

--- app/app/spiders/quotes_spider.py
+++ app/app/spiders/quotes_spider.py
@@ -5,9 +5,10 @@ class QuoteSpider(scrapy.Spider):
     name = 'quotes' #name of spider
 
     start_urls = [
-        'https://quotes.toscrape.com/'
+        'https://quotes.toscrape.com/page/1/'
     ]
 
+    page_number = 2
     def parse(self, response):
         quote_div = response.css('div.quote')
         
@@ -21,4 +22,11 @@ class QuoteSpider(scrapy.Spider):
             appItem['author'] = author
             appItem['tags'] = tags
 
+            # Pagination's concept
+            next_page = r'https://quotes.toscrape.com/page/'+ str(QuoteSpider.page_number) +'/'
+            # callback calls the parse function again
+            if QuoteSpider.page_number < 11 :
+                QuoteSpider.page_number = QuoteSpider.page_number + 1
+                yield response.follow(next_page,callback = self.parse)
+                
             yield appItem
